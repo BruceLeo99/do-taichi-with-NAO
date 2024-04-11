f949b15e60aa693552aa5987dca427671d9c54c6
Yixing Wang <yixingWang131@gmail.com>
2024-04-12 00:38:12 +0200

    source files upload

---
+++ Motivation Robot/translations/translation_en_US.ts
@@ -0,0 +1,245 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>3..2..1..</source>
+            <comment>Text</comment>
+            <translation type="obsolete">3..2..1..</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Ask for morning dance</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello</translation>
+        </message>
+        <message>
+            <source>Hello handsome, let's do a small dancing to boost your day! </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello handsome, let's do a small dancing to boost your day! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Do Tai chi</name>
+        <message>
+            <source>And now let's do some Tai Chi!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">And now let's do some Tai Chi!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Face detection/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Have a nice day</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Have an energetic and successful day papi!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Have an energetic and successful day papi!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/High Five/Cheers</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Cheers!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Cheers!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/High Five/Say</name>
+        <message>
+            <source>Cheers!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Cheers!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Morning Exercise/Ask for morning dance</name>
+        <message>
+            <source>Hello handsome, let's do a small dancing to boost your day! </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hello handsome, let's do a small dancing to boost your day! </translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello handsome, let's do a small exercise to boost your day! </source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello handsome, let's do a small exercise to boost your day! </translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Morning Exercise/Do Tai chi</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>And now let's do some Tai Chi!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">And now let's do some Tai Chi!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Morning Exercise/Ready?</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ready?3..2..1..</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ready?3..2..1..</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Morning Exercise/Say</name>
+        <message>
+            <source>Say something twice test </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Say something twice test </translation>
+        </message>
+        <message>
+            <source>You look awesome today!  </source>
+            <comment>Text</comment>
+            <translation type="obsolete">You look awesome today!  </translation>
+        </message>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Ready?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Ready?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Open Arm/Nice Job</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>You did a very nice job!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">You did a very nice job!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Open Arm/Say</name>
+        <message>
+            <source>That was im pressive!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That was im pressive!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Open Arm/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>You did a very nice job!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">You did a very nice job!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Open Arm/That was impressive</name>
+        <message>
+            <source>That was im pressive!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That was im pressive!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>That was impressive!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">That was impressive!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>What's up?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What's up?</translation>
+        </message>
+        <message>
+            <source>Hey, how are you doing?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Hey, how are you doing?</translation>
+        </message>
+        <message>
+            <source>Wanna see me dance?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Wanna see me dance?</translation>
+        </message>
+        <message>
+            <source>Say something twice test </source>
+            <comment>Text</comment>
+            <translation type="obsolete">Say something twice test </translation>
+        </message>
+        <message>
+            <source>Well done!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Well done!</translation>
+        </message>
+        <message>
+            <source>GG</source>
+            <comment>Text</comment>
+            <translation type="obsolete">GG</translation>
+        </message>
+        <message>
+            <source>That was im pressive!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">That was im pressive!</translation>
+        </message>
+        <message>
+            <source>Have an energetic and successful day papi!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Have an energetic and successful day papi!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>I will dance until Leo comes back </source>
+            <comment>Text</comment>
+            <translation type="obsolete">I will dance until Leo comes back </translation>
+        </message>
+        <message>
+            <source>Cheers!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Cheers!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/test box/Say</name>
+        <message>
+            <source>What's up?</source>
+            <comment>Text</comment>
+            <translation type="obsolete">What's up?</translation>
+        </message>
+    </context>
+</TS>
